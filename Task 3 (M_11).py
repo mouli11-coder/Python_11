1e3ee2a3e4389cf7d3eaa560dd94bd4a726e23f9
MOULI.R <thalamouli25@gmail.com>
2026-08-19 22:00:10 +0530

    Add files via upload

---
+++ Task 3 (M_11).py
@@ -0,0 +1,69 @@
+#TASK '3'  -  LOOP
+'''
+#1(1 to 10)
+for m in range(11):
+    print(m,end="")
+
+#2(10 to 1)
+for s in range(10,0,-1):
+     print(s,end="")
+
+#3( even number)
+for m in range(2,21,2):
+     print(m)
+
+#4(odd number)
+for m in range(1,21,2):
+    print(m)
+
+#5(mutli of 5)
+for m in range(5,51,5):
+    print(m)
+# OR
+num = 5  
+for i in range(1, 11):
+      print(f"{num} x {i} = {num * i}")
+
+#6(sum of 1 to 100)
+
+num=1
+for m in range(0,100):
+    print(f"{num} + {m} = {num+m}")
+#0R
+total_sum = 0
+for i in range(1, 101):
+    total_sum += i
+print(f"Total Sum: {total_sum}")
+
+
+#7(sum of even(upto 50))
+
+even_s= 0
+for m in range(2,51,2):
+    even_s += m
+    print(f"EVEN SUM: {even_s}")
+
+even_sum = 0
+for i in range(2, 51, 2):
+    even_sum += i
+print(f"Sum of Evens: {even_sum}")
+
+#8(string)
+m="PYTHON"
+for i in m:
+    print(i,end="")
+
+#9(counter)
+
+
+
+
+
+#10(square number)
+
+for m in range(1, 11):
+    print(f"Square of {m} is {m ** 2}")'''
+
+
+
+
